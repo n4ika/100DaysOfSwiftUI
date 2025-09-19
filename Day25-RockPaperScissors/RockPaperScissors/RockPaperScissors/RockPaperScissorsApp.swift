337bf452ba86715f2ff0e6760d5b0794c92b3b43
Naïka Estriplet <naika.c.3113@gmail.com>
2025-09-18 21:16:18 -0400

    Add files via upload

---
+++ Day25-RockPaperScissors/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Naïka Estriplet on 2025-09-18.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
